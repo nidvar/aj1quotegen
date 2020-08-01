ee320d6f14448b3a0a7301647dbfa588a6c47d63
Jay Kim <djayshine87@hotmail.com>
2020-08-01 11:18:33 +1000

    generate quotes

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('quote generator')
+const myFunc = ()=>{
+    fetch('https://mysterious-basin-66083.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+    .then(a=>{
+        return a.json();
+    })
+    .then(a=>{
+        console.log(a.quoteAuthor)
+        console.log(a.quoteText)
+        console.log(a.quoteAuthor)
+        display_quote(a.quoteText,a.quoteAuthor)
+    })
+    .catch(err=>{
+        myFunc();
+        console.log(err)
+    })
+}
+
+myFunc();
+
+const display_quote=(quote, author)=>{
+    const p = document.createElement('p');
+    p.innerHTML = `<p>${quote}</p><br /><p><i>${author}</i></p>`
+    document.getElementById('output').appendChild(p)
+}
+
+document.getElementById('new_quote').addEventListener('click',()=>{
+    document.getElementById('output').textContent = '';
+    myFunc();
+})
